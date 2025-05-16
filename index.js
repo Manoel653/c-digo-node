100be4372b83292237f115bfc4922d6c057c0fd1
Manoel <manofilhofer123@gmail.com>
2025-05-16 15:38:28 -0300

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const express = require('express');
-const app = express();
-
 app.get('/', (req, res) => {
   res.send('Teste');
 });
@@ -15,6 +12,50 @@ app.get('/features', (req, res) => {
 
 app.get('/contact', (req, res) => {
   res.send('Teste 4');
+});
+
+app.get('/api/usuario', (req, res) => {
+  res.json({
+    nome: 'João',
+    idade: 25,
+    email: 'joao@teste.com'
+  });
+});
+
+app.get('/api/protudo1', (req, res) => {
+  res.json({
+    id: 1,
+    nome: 'sapato1',
+    preco: 300,00,
+    descricao: 'sapato'
+  });
+});
+
+app.get('/api/produto2', (req, res) => {
+  res.json({
+    id: 2,
+    nome: 'sapato2',
+    preco: 150.00,
+    descricao: 'sapato'
+  });
+});
+
+app.get('/api/produto3', (req, res) => {
+  res.json({
+    id: 3,
+    nome: 'calça preta',
+    preco: 320.00,
+    descricao: 'calça'
+  });
+});
+
+app.get('/api/produto4', (req, res) => {
+  res.json({
+    id: 4,
+    nome: 'calça azul',
+    preco: 200.00,
+    descricao: 'calça'
+  });
 });
 
 app.listen(3000, () => {
